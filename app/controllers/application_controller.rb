a9d139bc6e45d847f115837388940de817db8384
Mason <mwhite90@chicagobooth.edu>
2026-05-23 18:41:12 -0500

    Fully Built Out Sign In And Out Page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,16 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  layout :layout_by_resource
+
+  private
+  def layout_by_resource
+    if devise_controller? && !user_signed_in?
+      "auth"
+    else
+      "application"
+    end
+  end
+
 end
