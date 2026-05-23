2faf2e65bc8230b495b0d1a70563c84b366eb1f3
Mason <268391644+mason-white-dev@users.noreply.github.com>
2026-05-23 16:46:22 -0500

    Bulding out basic Devise files, and adding a login dashboard index page

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,35 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  
+  # =========================================================================
+  # Devise Routes for :users Cheat Sheet
+  # =========================================================================
+  #
+  # SESSIONS (Log In / Log Out)
+  #   GET    /users/sign_in              => devise/sessions#new        (new_user_session_path)
+  #   POST   /users/sign_in              => devise/sessions#create     (user_session_path)
+  #   DELETE /users/sign_out             => devise/sessions#destroy    (destroy_user_session_path)
+  #
+  # PASSWORDS (Reset Passwords)
+  #   GET    /users/password/new         => devise/passwords#new       (new_user_password_path)
+  #   POST   /users/password             => devise/passwords#create    (user_password_path)
+  #   GET    /users/password/edit        => devise/passwords#edit      (edit_user_password_path)
+  #   PATCH  /users/password             => devise/passwords#update    (user_password_path)
+  #   PUT    /users/password             => devise/passwords#update
+  #
+  # REGISTRATIONS (Sign Up / Edit Account / Delete Account)
+  #   GET    /users/sign_up              => devise/registrations#new   (new_user_registration_path)
+  #   POST   /users/sign_up              => devise/registrations#create(user_registration_path)
+  #   GET    /users/edit                 => devise/registrations#edit  (edit_user_registration_path)
+  #   PATCH  /users                      => devise/registrations#update(user_registration_path)
+  #   PUT    /users                      => devise/registrations#update
+  #   DELETE /users                      => devise/registrations#destroy(user_registration_path)
+  #   GET    /users/cancel               => devise/registrations#cancel(cancel_user_registration_path)
+  #
+  # =========================================================================
+  devise_for :users
+
+  root "dashboard#index"
 end
