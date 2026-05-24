4922be78483e12559412bf7266845fe75d8fb25a
Mason <268391644+mason-white-dev@users.noreply.github.com>
2026-05-23 21:28:00 -0500

    Adding support for First and Last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  # Custom Validations
+  # Ensures that users cannot sign up or update their profile with blank names.
+  # If left blank, Devise will automatically block the form submission and show an error.
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
