c6ac71f9367d9573c5883b476d97ae056d97a6a7
Mason <268391644+mason-white-dev@users.noreply.github.com>
2026-05-23 22:29:59 -0500

    fixing linting issues

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
 
-  
+
   # =========================================================================
   # Devise Routes for :users Cheat Sheet
   # =========================================================================
