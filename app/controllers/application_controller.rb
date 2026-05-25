290ef77bfc04ed02a422f73f48ffed1b34ec6db8
Mason <268391644+mason-white-dev@users.noreply.github.com>
2026-05-25 09:16:41 -0500

    Removing First And Last Name logic from Users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,13 +8,8 @@ class ApplicationController < ActionController::Base
   # Require user authentication through Devise for all actions by default.
   before_action :authenticate_user!
 
-  # Ensure custom fields (like first_name, last_name) are permitted through Strong Parameters
-  # whenever a Devise controller handles user registration or profile updates (see below method).
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
   # Dynamically determine the layout to use based on the controller and user state (see below method).
   layout :layout_by_resource
-
 
   private
   # Use the "auth" layout for Devise controllers (like login/signup) when the user
@@ -25,13 +20,5 @@ class ApplicationController < ActionController::Base
     else
       "main"
     end
-  end
-
-  # Custom parameter handler for Devise controllers.
-  # This allows the "first_name" and "last_name" fields to be processed
-  # along with the standard email/password fields during sign up and updates.
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :first_name, :last_name ])
-    devise_parameter_sanitizer.permit(:account_update, keys: [ :first_name, :last_name ])
   end
 end
